e3fb051047e69a7ea5e3c275736b6e2a4e738f98
Tomasz <slabiak.tomasz@gmail.com>
2019-06-04 08:33:25 +0200

    add getAllUnreadNotifications function to NotificationRepository

--- src/main/java/com/example/slabiak/appointmentscheduler/dao/NotificationRepository.java
+++ src/main/java/com/example/slabiak/appointmentscheduler/dao/NotificationRepository.java
@@ -2,6 +2,12 @@ package com.example.slabiak.appointmentscheduler.dao;
 
 import com.example.slabiak.appointmentscheduler.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    @Query("select N from Notification N join N.user u where u.id = :userId and N.isRead=false")
+    List<Notification> getAllUnreadNotifications(@Param("userId")int userId);
 }
