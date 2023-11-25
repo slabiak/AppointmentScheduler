0fa2227d00682397c2334c2c219b5e77b2e5416f
Wesley L. Pinheiro <wesleylpinheiro@gmail.com>
2023-11-25 17:50:01 -0300

    refactoring of code smells
    after an analisys with PMD, we found  long method & duplicated code. those where eliminated with extract method technique.

--- src/main/java/com/example/slabiak/appointmentscheduler/model/DayPlan.java
+++ src/main/java/com/example/slabiak/appointmentscheduler/model/DayPlan.java
@@ -19,34 +19,50 @@ public class DayPlan {
         List<TimePeroid> breaks = getBreaks();
 
         if (!breaks.isEmpty()) {
-            ArrayList<TimePeroid> toAdd = new ArrayList();
-            for (TimePeroid break1 : breaks) {
-                if (break1.getStart().isBefore(workingHours.getStart())) {
-                    break1.setStart(workingHours.getStart());
-                }
-                if (break1.getEnd().isAfter(workingHours.getEnd())) {
-                    break1.setEnd(workingHours.getEnd());
-                }
-                for (TimePeroid peroid : timePeroidsWithBreaksExcluded) {
-                    if (break1.getStart().equals(peroid.getStart()) && break1.getEnd().isAfter(peroid.getStart()) && break1.getEnd().isBefore(peroid.getEnd())) {
-                        peroid.setStart(break1.getEnd());
-                    }
-                    if (break1.getStart().isAfter(peroid.getStart()) && break1.getStart().isBefore(peroid.getEnd()) && break1.getEnd().equals(peroid.getEnd())) {
-                        peroid.setEnd(break1.getStart());
-                    }
-                    if (break1.getStart().isAfter(peroid.getStart()) && break1.getEnd().isBefore(peroid.getEnd())) {
-                        toAdd.add(new TimePeroid(peroid.getStart(), break1.getStart()));
-                        peroid.setStart(break1.getEnd());
-                    }
-                }
-            }
-            timePeroidsWithBreaksExcluded.addAll(toAdd);
+            adjustBreaksWithinWorkingHours(timePeroidsWithBreaksExcluded);
             Collections.sort(timePeroidsWithBreaksExcluded);
         }
 
-
         return timePeroidsWithBreaksExcluded;
     }
+
+    private void adjustBreaksWithinWorkingHours(List<TimePeroid> timePeroidsWithBreaksExcluded) {
+        ArrayList<TimePeroid> toAdd = new ArrayList<>();
+        for (TimePeroid break1 : getBreaks()) {
+            adjustBreakStart(break1);
+            adjustBreakEnd(break1);
+
+            for (TimePeroid peroid : timePeroidsWithBreaksExcluded) {
+                handleBreakOverlap(break1, peroid, toAdd);
+            }
+        }
+        timePeroidsWithBreaksExcluded.addAll(toAdd);
+    }
+
+    private void adjustBreakStart(TimePeroid break1) {
+        if (break1.getStart().isBefore(workingHours.getStart())) {
+            break1.setStart(workingHours.getStart());
+        }
+    }
+
+    private void adjustBreakEnd(TimePeroid break1) {
+        if (break1.getEnd().isAfter(workingHours.getEnd())) {
+            break1.setEnd(workingHours.getEnd());
+        }
+    }
+
+    private void handleBreakOverlap(TimePeroid break1, TimePeroid peroid, List<TimePeroid> toAdd) {
+        if (break1.getStart().equals(peroid.getStart()) && break1.getEnd().isAfter(peroid.getStart()) && break1.getEnd().isBefore(peroid.getEnd())) {
+            peroid.setStart(break1.getEnd());
+        }
+        if (break1.getStart().isAfter(peroid.getStart()) && break1.getStart().isBefore(peroid.getEnd()) && break1.getEnd().equals(peroid.getEnd())) {
+            peroid.setEnd(break1.getStart());
+        }
+        if (break1.getStart().isAfter(peroid.getStart()) && break1.getEnd().isBefore(peroid.getEnd())) {
+            toAdd.add(new TimePeroid(peroid.getStart(), break1.getStart()));
+            peroid.setStart(break1.getEnd());
+        }
+    }    
 
     public TimePeroid getWorkingHours() {
         return workingHours;
